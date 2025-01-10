3ef07079d385e51410e09a0aac3d3c9b47e25562
Simran <simrankaurgulati2006@gmail.com>
2025-01-10 23:27:33 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.jsx';  // Correct import
-import './index.css';  // Your global styles
+import App from './App.jsx';  
+import './index.css'; 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
